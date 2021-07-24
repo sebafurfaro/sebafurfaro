555c0789208fee670d3e0aabadfdde431221cb35
sebafurfaro <sebafurfaro@gmail.com>
2021-07-24 20:05:15 -0300

    aparece boton finalizar compra

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -10,3 +10,4 @@ const CartWidget = () => {
 };
 
 export default CartWidget;
+
--- src/hooks/StockCounter/StockCounter.js
+++ src/hooks/StockCounter/StockCounter.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
+import { FaMinus, FaPlus } from 'react-icons/fa';
 import "./StockCounter.scss";
 
 function StockCounter({ stock, onAdd }) {
   const [toAdd, setToAdd] = useState(stock ? 1 : 0);
 
+  const [showLink, setShowLink] = useState(false);
+
+  const handleLink = () => setShowLink(true);
+
+  const multipleAction = () => {
+    onAdd(toAdd);
+    handleLink();
+  }
+  
   return (
     <div className="StockCounter">
       <div className="d-flex justify-content-between counter">
@@ -12,21 +23,23 @@ function StockCounter({ stock, onAdd }) {
           className="stockButton"
           onClick={() => setToAdd(toAdd - 1 ? toAdd - 1 : toAdd)}
         >
-          -
+          <FaMinus />
         </Button>
         <div className="stock-number">{toAdd}</div>
         <Button
           className="stockButton"
           onClick={() => setToAdd(toAdd === stock ? toAdd : toAdd + 1)}
         >
-          +
+          <FaPlus/>
         </Button>
       </div>
-      <Button className="cartButton" onClick={() => onAdd(toAdd)}>
+      <Button className="cartButton" onClick={multipleAction}>
         Añadir
       </Button>
+      {showLink ? <Link to="/cart" className="btn finishShop">Terminar compra</Link> : null}
     </div>
   );
+  
 }
 
 export default StockCounter;
